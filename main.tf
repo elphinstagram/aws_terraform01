82082e5d5181cecedccc504900ceeee1c404dd65
Sachiko Nagao <elphinstagram@google.com>
2022-12-14 21:21:31 +0900

    Add Security Group

--- main.tf
+++ main.tf
@@ -51,3 +51,31 @@ resource "aws_route_table_association" "terra_public_asc" {
   subnet_id      = aws_subnet.terra_public_subnet.id
   route_table_id = aws_route_table.terra_public_rt.id
 }
+
+# Security Group
+resource "aws_security_group" "terra_sg" {
+  name        = "terra_sg"
+  description = "terra_sg"
+  vpc_id      = aws_vpc.terra_vpc.id
+
+  ingress {
+    description = "terra_sg inbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "terra_sg"
+  }
+}
